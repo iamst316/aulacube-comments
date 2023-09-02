1f0fbaad2e18719702398e5c222db7980d20374a
Shubham Tandon <iamshubham316@gmail.com>
2023-09-02 11:42:56 +0530

    basic struct

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export default function(){
+
+    return(<>
+        <div class="welcome">
+            <div class="h1">Welcome To</div>
+            <div class="h2">AulaCube Comments</div>
+        </div>
+    </>)
+}
